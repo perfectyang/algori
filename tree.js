020a8278126509fb1f5a2fa205b4574eaf69adc9
杨国唯 <yangguowei@yangguoweideMac-mini.local>
2020-04-20 15:08:10 +0800

    测试

--- tree.js
+++ tree.js
@@ -70,7 +70,7 @@ class Tree {
     node.right && this.binarySearchNode(node.right, cb)
   }
 
-  delNode (key) {
+  remove (key) {
     if (!this.root) return -1
     // 先找到要删除的节点
     let current = this.root
@@ -138,118 +138,49 @@ class Tree {
   }
 
   getSuccessor (delNode) {
+    console.log('delNode', delNode)
     let successor = delNode.right
     let successorParent = delNode.right
 
     while (successor.left !== null) {
+      console.log('进来吗')
       successorParent = successor
       successor = successor.left
     }
-
+    console.log('successor', successor)
     if (delNode.right !== successor) {
+      console.log('进来这里', delNode)
       successorParent.left = successor.right
       successor.right = delNode.right
     }
-
     return successor
-
   }
-
-
-
-  // delNode (key) {
-  //   if (this.size === 0) {
-  //     return -1
-  //   } else {
-  //     let node = this.root
-  //     let findNode
-  //     let parentNode
-  //     while (node) {
-  //       if (node.key === key) {
-  //         findNode = node
-  //         break
-  //       } else if (key > node.key) {
-  //         parentNode = node
-  //         node = node.right
-  //       } else if (key < node.key) {
-  //         parentNode = node
-  //         node = node.left
-  //       }
-  //     }
-  //     if (findNode) { // 找到
-  //       console.log('找到节 点', findNode, parentNode)
-  //       if (!findNode.left && !findNode.right) {
-  //         if (!parentNode) { // 说明找到的是根节点
-  //           this.root = null
-  //           this.size -= 1
-  //           return
-  //         }
-  //         // 1. 如果删除的节点为叶子节点，即它的左子节点指针和右子节点指针都为空时，则可以直接删掉该节点，并不会影响整棵树的结构。
-  //         if (findNode.key > parentNode.key) { // 删除节点是在父节点的右边
-  //           parentNode.right = null
-  //         } else { // 删除节点是在父节点的左边, 则将节点的左边置为null
-  //           parentNode.left = null
-  //         }
-  //         this.size -= 1
-  //       } else if (findNode.left && findNode.right) {
-  //         // 2. 如果删除的节点有两个子节点
-  //         if (!parentNode) { // 说明是删除根节点
-  //           let curNode = findNode.left
-  //           while (curNode.right) {
-  //             curNode = curNode.right
-  //           }
-  //           return
-  //         }
-  //       } else {
-  //         if (!parentNode) { // 说明是删除根节点
-  //           if (findNode.left) {
-  //             this.root = findNode.left
-  //           } else {
-  //             this.root = findNode.right
-  //           }
-  //           this.size -= 1
-  //           return
-  //         }
-  //         // 3. 如果删除的节点只有一个子节点(左子节点或右子节点)，则直接将子节点提升到被删除的节点位置
-  //         if (findNode.key > parentNode.key) { // 找到的节点是父节点的右边
-  //           if (findNode.left) {
-  //             parentNode.right = findNode.left
-  //           } else {
-  //             parentNode.right = findNode.right
-  //           }
-  //         } else { // 找到的节点是父节点的左边
-  //           if (findNode.left) {
-  //             parentNode.left = findNode.left
-  //           } else {
-  //             parentNode.left = findNode.right
-  //           }
-  //         }
-  //       }
-  //     } else { // 没找到
-  //       console.log('没找到', findNode, parentNode)
-  //     }
-  //   }
-  // }
-
 }
 
 const t = new Tree()
 function print () {
   let str = ''
   t.binarySearch((node) => {
-    str += String(node.key)
+    str += String(node.key) + '-'
   })
   console.log(str)
 }
 
-t.insert(2, 2)
-t.insert(1, 1)
-t.insert(10, 333)
-t.insert(3, 3)
-console.log(t.get(10))
+t.insert(11, 11)
+t.insert(9, 9)
+t.insert(30, 30)
+t.insert(8, 8)
+t.insert(10, 10)
+t.insert(5, 5)
+t.insert(25, 25)
+t.insert(22, 22)
+t.insert(26, 26)
+t.insert(34, 34)
+t.insert(31, 31)
+t.insert(33, 33)
+t.insert(35, 35)
 print()
-
+t.remove(10)
+print()
 // t.delNode(6)
 // console.log(t)
-   11
-9     19
