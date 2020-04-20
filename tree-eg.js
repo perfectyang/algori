41ab64e52fe46453fc3f3458b8e60cbe9cc63e43
杨国唯 <yangguowei@yangguoweideMac-mini.local>
2020-04-20 15:42:44 +0800

    添加广度遍历和深度遍历例子

---
+++ tree-eg.js
@@ -0,0 +1,49 @@
+const json = {
+  state: 1,
+  children: [
+    {
+      state: 2,
+      children: [
+        {
+          state: '2-1'
+        },
+        {
+          state: '2-2'
+        }
+      ]
+    },
+    {
+      state: 3,
+      children: [
+        {
+          state: '3-1'
+        },
+        {
+          state: '3-2'
+        }
+      ]
+    }
+  ]
+}
+
+// 深度遍历树
+
+function trvaseDeep (obj) {
+  console.log(obj.state)
+  if (obj.children) {
+    obj.children.forEach(cur => trvaseDeep(cur))
+  }
+}
+
+// 广度遍历
+function trvaseBroad (node) {
+  let tem = [node]
+  let current = tem.pop()
+  while (current) { // 进来循环区
+    console.log('current', current.state)
+    tem = tem.concat(current.children || [])
+    current = tem.pop()
+  }
+}
+trvaseDeep(json)
+trvaseBroad(json)
